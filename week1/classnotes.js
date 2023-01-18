ca2c559f7385856e9c887dc6677c584c393739db
Jocelyn Martin <jmartin@mcad.edu>
2023-01-17 19:56:12 -0600

    notes

---
+++ week1/classnotes.js
@@ -0,0 +1,47 @@
+// visit office hrs to set up automatic save
+// use live server
+// how did he commit from vs code?
+// put script tag either at end of body or head
+// cmd slash to comment out a line
+// dev console shortcut is cmd option j
+
+/* block comments */
+// single line comments
+
+// use console log for buckets of information
+    // variables
+    // var isItTuesday = true; // boolean
+// camel case is for legibility
+// snake case
+
+// simplest form of data is true/false = boolean
+// mixed datatype
+// string
+    // alphanumeric characters
+    // concatination aka concat = a string + another string
+    // a space is also considered a string " " + myGithub
+
+// use consol logs for debugging
+
+// window.alert(); will open a modal display whatever is in parentheses
+    // window.prompt converts everything to strings
+    // console.log((inputString * 1) + myNumber); ??????
+        // way to convert string >> variable is to mulitply by 1
+
+// NUMBERS
+    // var myNumber = 42; 
+
+// MAKING DECISIONS aka conditionals
+    // if () {}
+        // condition is in the parentheses
+        // runs if condition is true
+    // conditional operators
+        // equality operator
+                /* 
+                if (5 === (3 + 2)) {
+                    console.log("they're the same");
+                }
+                */
+            // use === for equality checking 
+            // only two == is more "loose"
+        // >= greater than or equal to
