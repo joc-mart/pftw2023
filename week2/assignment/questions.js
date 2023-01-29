8e8882202221fa648fce15f553d5a04eb216c9eb
Jocelyn Martin <jmartin@mcad.edu>
2023-01-29 15:17:15 -0600

    create quiz files

---
+++ week2/assignment/questions.js
@@ -0,0 +1,39 @@
+// trivia question and answers
+/* const questionOne = {
+    question: "blah blah",
+    answer: "blah blah"
+}
+const questionTwo = {
+    question: "blah",
+    answer: "blah"
+}
+*/
+
+var questions = [
+    {
+        question: "What is the name of the longest mountain range in the world that stretches over 56,000km?",
+        answer: "The Mid-Oceanic Ridge"
+    },
+    {
+        question: "What is the world's largest living structure?",
+        answer: "The Great Barrier Reef"
+    },
+    {
+        
+    }
+]
+
+
+
+
+
+
+
+// random question prompt
+var randomQuestion = window.prompt("");
+
+// "you answered" alert + response 
+
+
+// The correct answer was + answer property
+
