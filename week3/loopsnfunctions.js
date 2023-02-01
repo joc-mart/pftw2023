776eaf5480087c3503b5a62ca842703dba43cc63
Jocelyn Martin <jmartin@mcad.edu>
2023-02-01 17:18:50 -0600

    functions examples

--- week3/loopsnfunctions.js
+++ week3/loopsnfunctions.js
@@ -83,6 +83,7 @@ const main = () => {
 } // picks up <div id="myTarget"></div>
 */
 
+/*
 const colors = ["magenta", "yellow", "black", "cyan"];
 for (let loopVar = 0; loopVar < colors.length; loopVar++){
     console.log("loop var", colors[loopVar]);
@@ -103,4 +104,77 @@ let myNumber = 0
 while(myNumber < 200) {
     console.log("myNumber", myNumber);
     myNumber += 10;
+}
+*/
+
+/*
+"indigo", "violet"]
+function setup() {
+    createCanvas(800, 400);
+    background(0);
+}
+
+function draw() {
+    noLoop(); //draw will not repeat
+    noStroke();
+    fill(255);
+    for(let startX = 0; startX <= 800; startX += 10) {
+        rect(startX, 0, 3, 400);
+    }
+}
+*/
+
+/*
+const colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"]
+let colorIndex = 0;
+let startX = 0;
+function setup() {
+    createCanvas(800, 400);
+    background(0);
+}
+*/ 
+/*
+function draw() {
+    noLoop(); //draw will not repeat
+    
+    for(let colorIndex = 0; colorIndex < colors.length; colorIndex ++) {
+        fill(colors[colorIndex]);
+        rect(colorIndex * 10, 0, 3, 400);
+    }
+}
+*/
+
+
+/* ERROR */
+/*
+function draw () {
+
+    fill(colors[colorIndex]);
+    rect(startX, 0, 10, 400);
+    if(colorIndex < colors.length) {
+        colorIndex++;
+    }
+    else {
+        colorIndex = 0;
+    }
+    startX += 20;
+}
+*/
+/* ERROR */
+
+
+function setup () {
+    createCanvas(800, 400);
+    background(0)
+
+}
+
+function makeCircles(circlex, circley) {
+    noFill();
+    stroke(255);
+    rect(circlex, circley, 20, 20);
+}
+
+function draw () {
+    makeCircles(random(0, 800,), random(0, 400));
 }
