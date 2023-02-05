9b7ab5ab7e50801401e399f6a166639446f0aba2
Jocelyn Martin <jmartin@mcad.edu>
2023-02-05 16:06:53 -0600

    radial art

---
+++ week3/assignment/assignment3.js
@@ -0,0 +1,42 @@
+let rotateBy = 5;
+
+function setup() {
+    createCanvas(600, 600);
+    background(0);
+    angleMode(DEGREES);
+}
+function makeArm(rotateBy) {
+    let alt = Math.round(rotateBy / 360);
+    noFill();
+    stroke("#32CD32");
+    strokeWeight(2);
+    triangle(30, 75, 58, 20, 86, 75);
+
+    noFill();
+    stroke("#7045FF");
+    strokeWeight(2);
+    line(30, 20, 100, 175);
+
+    fill("yellow");
+    noStroke();
+    circle(10, 10, 5);
+
+    fill("yellow");
+    noStroke();
+    circle(180, 180, 5);
+
+    noFill();
+    stroke("#FF45D6");
+    quad(138, 131, 186, 120, 169, 163, 130, 176);
+
+}
+
+function draw() {
+    translate(300, 300);
+    rotate(rotateBy) // rotates the canvas
+    makeArm(rotateBy);
+    rotateBy += 11;
+}
+function mousePressed() {
+    noLoop(); // what if I want it to start again?
+}
