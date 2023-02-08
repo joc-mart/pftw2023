1f56d196c2b07cf41ebdd4eb3500238388e57248
Jocelyn Martin <jmartin@mcad.edu>
2023-02-07 19:31:55 -0600

    more notes

--- week4/classnotes/classnotes.js
+++ week4/classnotes/classnotes.js
@@ -16,3 +16,20 @@ myDivTarget.addEventListener("click", () =>  {
 })
 // click, mouseover, mouseout
 
+
+// use . for classes # for ids for document.querySelector - find the first to match within the class
+/*
+const coolDivs = document.getElementsbyClassName("cool-divs");
+console.log("cool divs", coolDivs);
+const coolDivSingle = document.querySelector(".cool-divs");
+coolDivSingle.style.fontSize = "2rem";
+*/
+
+const coolDivSingle = document.querySelector(".cool-divs");
+coolDivSingle.style.fontSize = "2rem";
+coolDivSingle.addEventListener("mouseover", () => {
+    coolDivSingle.style.textAlign = "right";
+})
+
+// hover is not an event
+// 
