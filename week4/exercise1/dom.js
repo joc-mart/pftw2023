74489d5e0948c0e5a0061b2011277de3d65b2a75
Jocelyn Martin <jmartin@mcad.edu>
2023-02-09 13:04:20 -0600

    exercise 1 demo

---
+++ week4/exercise1/dom.js
@@ -0,0 +1,17 @@
+// select empty div and assign to a variable
+const myEmptyDiv = document.querySelector("#myEmptyDiv");
+
+// put h1 inside 
+// create new element
+let heading = document.createElement("h1");
+heading.innerHTML = "hello class! It is a snowy day"; 
+heading.style.cursor = "pointer";
+heading.addEventListener("click", handleHeadingClick);
+
+myEmptyDiv.appendChild(heading); // attach to dom
+
+function handleHeadingClick () {
+    console.log("Heading has been clicked");
+    document.body.style.backgroundColor = "magenta";
+}
+
