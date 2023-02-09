84d46f339b06afdd329d1c829b23b5b7dd7fe4a8
Jocelyn Martin <jmartin@mcad.edu>
2023-02-09 14:50:10 -0600

    random method

--- week4/exercise2/clicker.js
+++ week4/exercise2/clicker.js
@@ -1,18 +1,21 @@
 let rectX = 0;
 let rectWidth = 75;
-let rectY = 0;
+let rectY;
 let rectHeight = 75;
 
 let clickCount = 0
+let speed;
 
 function setup () {
     createCanvas(500, 500);
+    rectY = random(height - rectHeight);
+    speed = random(1, 3); // 1 is min, 3 is max
 }
 
 function draw () {
     background(155);
     drawShape();
-    rectX ++; // += 2 is x = x + 2
+    rectX += speed;
     if(rectX > width) {
         noLoop();
         text("your score was " + clickCount, 100, 300);
@@ -26,10 +29,12 @@ function mousePressed () {
     }
     
 }
-// why does it still say 
+// why does it still say hit when its clicking left or righ of square?
 
 function drawShape () {
     fill("purple");
     rect(rectX, rectY, rectWidth, rectHeight);
 
 }
+
+// random method
