3a190a8c6329fb674e4925317d65cd6fe476d49d
Jocelyn Martin <jmartin@mcad.edu>
2023-02-15 13:41:21 -0600

    edit color options

--- week5/exercise1/timers.js
+++ week5/exercise1/timers.js
@@ -1,6 +1,6 @@
 let blockX = 0;
 let blockY = 0;
-let blockColor = 255;
+let blockColor = 0;
 let drawTimer;
 const speed = 10;
 const distance = 2;
@@ -12,7 +12,7 @@ function setup () {
 }
 
 function drawBlock (x, y, color) {
-    fill(color || 255); // || means or
+    fill(color || 3, 90, 252); // || means or
     rect(x, y, 50, 50);
 }
 
@@ -21,7 +21,7 @@ function keyTyped () {
     if (isNaN(keyToNumber)){
         return;
     }
-    keyToNumber = map(keyToNumber, 1, 9, 1, 255);
+    keyToNumber = map(keyToNumber, 0, 9, 0, 255);
     console.log("converted number", keyToNumber);
     blockColor = keyToNumber;
     
@@ -35,7 +35,7 @@ drawTimer = window.setInterval(() => {
         blockY = 0;
         blockX += 50;
     }
-    if (blockY - 50 > height && blockX - 50 > width) {
+    if (blockY - 50 < height && blockX - 50 > width) {
         window.clearInterval(drawTimer);
         console.log("done");
         // clears timer
@@ -44,3 +44,4 @@ drawTimer = window.setInterval(() => {
 // went one past
 // keyTyped (key) vs keyPressed (keyCode)
 
+// colors 3, 252, 190
