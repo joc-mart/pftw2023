6387e6ecacdbea13b26fe9dab1d935a7570bdca9
Jocelyn Martin <jmartin@mcad.edu>
2023-02-14 19:41:52 -0600

    update class demo

--- week5/class/class.js
+++ week5/class/class.js
@@ -51,4 +51,34 @@ console.log(myWhiteMouse.weight);
 myWhiteMouse.bump();
 */
 
+let startDotLocation = 50;
+function setup() {
+    createCanvas(1000, 800);
+    background(0);
 
+}
+
+function draw () {
+    const myDot = new Dot(startDotLocation, 500, "blue");
+    myDot.show();
+    startDotLocation++;
+}
+
+class Dot {
+    constructor(startX, startY, color) {
+        this.x = startX;
+        this.y = startY;
+        this.color = color;
+    }
+    show () {
+        if(this.x >= 500) {
+            fill("red");
+        } else {
+            fill(this.color); 
+        }
+        
+        circle(this.x, this.y, 100);
+    }
+}
+
+// why use this?
