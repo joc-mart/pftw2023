6e0b545cdca38b1cbf17eb597c5b4b258d49e207
Jocelyn Martin <jmartin@mcad.edu>
2023-03-08 18:12:19 -0600

    week 8 class

---
+++ week8/class/class.js
@@ -0,0 +1,32 @@
+// v-model : create a variable 
+    // reflective
+    // .lazy
+
+const { createApp } = Vue;
+
+createApp ({
+    data() {
+        return {
+            nameInput: ""
+        }
+    }
+}).mount("#myApp");
+
+// v-for="option in ageOptions"
+// events
+// forms
+// array.prototype.filter()
+// assignment: add/delete items 
+// setup node environment
+    // open terminal
+    // node -v : shows you have node
+    // let him know sat if doesn't work
+
+// event handling
+// v-on is same as @ 
+    // v-on:click="handler" or @click="handler"
+    // inline or method
+// form input bindings
+    // v-model
+
+// v-bind
