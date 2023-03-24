55d49da53683c64ddd139b49a186648fcc12ac26
Jocelyn Martin <jmartin@mcad.edu>
2023-03-23 20:31:02 -0500

    exercise 2

--- week9/component/src/App.vue
+++ week9/component/src/App.vue
@@ -1,7 +1,31 @@
 <script setup>
+ const camelids = [
+    {
+      commonName: "Llama",
+      binomialName: "Lama glama",
+      order: "Artiodactyla",
+      family: "Camelidae",
+      genus: "Lama",
+      species: "L. glama",
+      image: "https://upload.wikimedia.org/wikipedia/commons/thumb/bb9/Llama_lying_down.jpg/220px-Llama_lying_down.jpg"
+    },
+    {
+      commonName: "Alpaca",
+      binomialName: "Vicugna pacos",
+      order: "Artiodactyla",
+      family: "Camelidae",
+      genus: "Vicugna",
+      species: "V. pacos",
+      image: "https://upload.wikimedia.org/wikipedia/commons/thumb/ddb/Alpaca_%2831562329701%29.jpg/440px-Alpaca_%2831562329701%29jpg"
+    },
+  ]
+
 </script>
 
 <template>
+  <div>
+    <p v-for="animal in camelids" v-bind:key="animal.commonName">{{animal.commonName}}</p>
+  </div>
 </template>
 
 <style scoped>
