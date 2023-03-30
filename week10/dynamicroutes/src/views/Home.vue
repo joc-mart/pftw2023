3946e306a16f033ee6cb0d28550a2098f39c8840
Jocelyn Martin <jmartin@mcad.edu>
2023-03-30 11:40:33 -0500

    10.2 demo

--- week10/dynamicroutes/src/views/Home.vue
+++ week10/dynamicroutes/src/views/Home.vue
@@ -11,3 +11,29 @@ console.log("Music List", MusicList);
         </div>
     </div>
 </template>
+
+<style scoped>
+    .song-container {
+        display: flex;
+        flex-direction: row;
+        flex-wrap: wrap;
+        justify-content: space-between;
+    }
+    .song-list-item {
+        width: auto;
+        padding: 10px;
+        margin: 10px;
+        border: 1px solid black;
+        border-radius: 4px;
+        background: pink;
+    }
+    .song-list-item a {
+        color: black;
+    }
+    .song-list-item:hover {
+        background: black;
+    }
+    .song-list-item:hover a {
+        color: white;
+    }
+</style>
--- week10/dynamicroutes/src/views/Song.vue
+++ week10/dynamicroutes/src/views/Song.vue
@@ -1,5 +1,34 @@
+<script setup>
+import { useRoute } from "vue-router"
+import MusicList from "../data/music-list.json"
+
+const route = useRoute();
+console.log(route.params.slug);
+const song = MusicList.find(song => {
+    return song.slug === route.params.slug;
+})
+</script>
+
 <template>
-    <div>
-        Song
+    <div class="song-detail" :class="{
+    rising: song.rank > song.position.positionLastWeek,
+    falling: song.rank < song.position.positionLastWeek,
+    same: song.rank === song.position.positionLastWeek}">
+        <h1>{{song.rank}}</h1>
+        <h2>{{song.title}}</h2>
+        <img :src="song.cover" :alt="song.title">
+        <p>Artist: {{song.artist}}</p>
     </div>
 </template>
+
+<style scoped>
+    .song-detail.rising {
+        border: 1px solid green;
+    }
+    .song-detail.falling {
+        border: 1px solid red;
+    }
+    .song-detail.same {
+        border: 1px solid yellow;
+    }
+</style>
