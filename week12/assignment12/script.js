f49078d2dcbce544f5c8968ccb5e4c16865cbc42
Jocelyn Martin <jmartin@mcad.edu>
2023-04-15 16:31:36 -0500

    piano fixed

--- week12/assignment12/script.js
+++ week12/assignment12/script.js
@@ -16,46 +16,69 @@ function preload() {
 }
 
 function setup() {
-    createCanvas(100, 100);
-    
+    createCanvas(0, 0);
+
     button = createButton("do");
-    button.mousePressed(play());
+    button.mousePressed(togglePlaying1);
+    button.style('background-color', "red");
 
     button2 = createButton("re");
-    button2.mousePressed(play());
+    button2.mousePressed(togglePlaying2);
+    button2.style('background-color', "orange");
 
-    // button = createButton("mi");
-    // button.mousePressed(togglePlaying);
+    button3 = createButton("mi");
+    button3.mousePressed(togglePlaying3);
+    button3.style('background-color', "yellow");
 
-    // button = createButton("fa");
-    // button.mousePressed(togglePlaying);
+    button4 = createButton("fa");
+    button4.mousePressed(togglePlaying4);
+    button4.style('background-color', "#1ac447");
 
-    // button = createButton("sol");
-    // button.mousePressed(togglePlaying);
+    button5 = createButton("sol");
+    button5.mousePressed(togglePlaying5);
+    button5.style('background-color', "#4882e0");
 
-    // button = createButton("la");
-    // button.mousePressed(togglePlaying);
+    button6 = createButton("la");
+    button6.mousePressed(togglePlaying6);
+    button6.style('background-color', "#a74ced");
 }
 
 function draw() {
     background(0);
 }
 
-// function togglePlaying() {
-//     // if (dos.isPaused()) {
-//     //     dos.play();
-//     //     // button.html("do");
-//     //     console.log("do is playing");
-//     // }
-//     // if (re.isPaused()) {
-//     //     re.play();
-//     //     // button.html("re");
-//     // }
-//     play();
-    
-    // re.play();
-    // mi.play();
-    // fa.play();
-    // sol.play();
-    // la.play();
-// }
+function togglePlaying1() {
+    if (!dos.isPlaying() || dos.isPlaying()) {
+        dos.play();
+    }
+}
+
+function togglePlaying2() {
+    if (!re.isPlaying() || re.isPlaying()) {
+        re.play();
+    }
+}
+
+function togglePlaying3() {
+    if (!mi.isPlaying() || mi.isPlaying()) {
+        mi.play();
+    }
+}
+
+function togglePlaying4() {
+    if (!fa.isPlaying() || fa.isPlaying()) {
+        fa.play();
+    }
+}
+
+function togglePlaying5() {
+    if (!sol.isPlaying() || sol.isPlaying()) {
+        sol.play();
+    }
+}
+
+function togglePlaying6() {
+    if (!la.isPlaying() || la.isPlaying()) {
+        la.play();
+    }
+}
