2afe5a59694ea6e481f1b57f975267d4d7627e7b
Jocelyn Martin <jmartin@mcad.edu>
2023-04-22 16:46:00 -0500

    tree sketch

--- week13/assignment13/sketch.js
+++ week13/assignment13/sketch.js
@@ -3,7 +3,7 @@ function setup() {
     createCanvas(windowWidth, windowHeight, WEBGL)
     angleMode(DEGREES)
 
-    noLoop()
+    // noLoop()
 }
 
 function draw() {
@@ -11,9 +11,26 @@ function draw() {
 
     translate(0, 200, 0)
 
+    rotateY(frameCount)
+
     branch(100)
 }
 
 function branch(len) {
     line(0, 0, 0, 0, -len, 0)
+
+    translate(0, -len, 0)
+
+    if (len > 10) {
+        for (var i = 0; i < 3; i++) {
+            rotateY(120)
+
+            push()
+
+            rotateZ(30)
+            branch(len * 0.6)
+
+            pop() // prevent loop from affecting each other
+        }
+    }
 }
